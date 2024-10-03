38e73c4306bbd226452590db20223181c670b734
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-03 19:20:57 +0600

    Update index.js

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -5,6 +5,8 @@ import { Col, Row } from "react-bootstrap";
 import Link from "next/link";
 import Image from "next/image";
 import SliderOne from "../../assets/ecommerceslider_1.jpg";
+import CameraImg from "../../assets/sameraone.jpg";
+import PcImage from "../../assets/pcollectionone.jpg";
 import BannerSlider from "./BannerSlider";
 import { Montserrat } from "next/font/google";
 
@@ -25,29 +27,32 @@ const Banner = () => {
             </Col>
             <Col xs={4}>
               <div className="banner-collection">
-                <div className="mb-4">
-                  <Link href="/">
+                <div className="collection-item mb-4">
+                  <Link href="/" className="collection-name">
+                    <span className={montserrat.className + " " + " "}>
+                      PC Gaming Collection
+                    </span>
+
                     <Image
                       width={512}
                       height={306}
-                      src={SliderOne}
+                      src={PcImage}
                       alt="slider-one"
                     />
                   </Link>
                 </div>
                 <div className="collection-item">
                   <Link href="/" className="collection-name">
+                    <span className={montserrat.className + " " + " "}>
+                      PC Gaming Collection
+                    </span>
+
                     <Image
                       width={512}
                       height={306}
-                      src={SliderOne}
-                      alt="slider-one"
+                      src={CameraImg}
+                      alt="Camera"
                     />
-                    <div className="collection-header">
-                      <h5 className={montserrat.className + " " + " "}>
-                        PC Gaming Collection
-                      </h5>
-                    </div>
                   </Link>
                 </div>
               </div>
