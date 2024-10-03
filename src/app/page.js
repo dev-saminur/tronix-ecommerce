f00504074258d80d683b49d101a8806d3a10d28a
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-03 21:54:37 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
-import Banner from "@/components/Banner";
 import "./styles/globals.scss";
+import Banner from "@/components/Banner";
 import Layout from "@/components/Layout/Layout";
+import Category from "@/components/Category";
 
 export default function Home() {
   return (
     <>
       <Layout>
         <Banner />
+        <Category />
       </Layout>
     </>
   );
