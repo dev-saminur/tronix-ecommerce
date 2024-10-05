3b703a4154f4ffe8c9d5de3596b32efc3e65b0db
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-05 17:42:33 +0600

    Update index.js

--- src/components/NewArrival/index.js
+++ src/components/NewArrival/index.js
@@ -5,7 +5,13 @@ import { Col, Row } from "react-bootstrap";
 import Smartphone from "../../assets/arrival/smartphone.jpg";
 import Image from "next/image";
 import Badge from "../helper/Badge/Badge";
+import { Poppins } from "next/font/google";
+import { MdStarRate } from "react-icons/md";
 
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
 const NewArrival = () => {
   return (
     <>
@@ -26,7 +32,23 @@ const NewArrival = () => {
                     <Badge title="new" alone={true} />
                   </div>
                 </div>
-                <div className="content">Content</div>
+                <div className="content">
+                  <h4 className={poppins.className + " " + " "}>
+                    USB Speaker Portable
+                  </h4>
+                  <h3>$299.00</h3>
+                  <div className="review d-flex align-items-center">
+                    <div className="rating d-flex align-items-center">
+                      <MdStarRate />
+                      <span className={poppins.className + " " + " "}>5.0</span>
+                    </div>
+                    <div className="sold-out">
+                      <span className={poppins.className + " " + " "}>
+                        sold out
+                      </span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </Col>
             <Col md={4}>Two</Col>
