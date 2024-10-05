a1815951660b0d98e2d8b29a6c0f433d025da638
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-05 22:57:53 +0600

    Update FlashProduct.js

--- src/components/FlashSale/FlashProduct.js
+++ src/components/FlashSale/FlashProduct.js
@@ -27,82 +27,24 @@ const FlashProduct = () => {
               </div>
             </div>
             <div className="content">
-              <h4 className={poppins.className + " " + " "}>Headphone</h4>
-              <h3>$120</h3>
-              <div className="review d-flex align-items-center">
-                <div className="rating d-flex align-items-center">
-                  icon
-                  <span className={poppins.className + " " + " "}>5.0</span>
-                </div>
-                <div className="sold-out">
-                  <span className={poppins.className + " " + " "}>
-                    sold out
-                  </span>
-                </div>
+              <h4 className={poppins.className + " " + " "}>
+                Wireless Headphone
+              </h4>
+              <div className="price d-flex align-items-center">
+                <h3 className={poppins.className + " " + " "}>
+                  {" "}
+                  <del>$150</del>{" "}
+                </h3>
+                <h3 className={poppins.className + " " + " "}>- $150</h3>
+              </div>
+              <div className="sold-out">
+                <span className={poppins.className + " " + " "}>sold out</span>
               </div>
             </div>
           </div>
         </Col>
-        <Col md={4}>
-          <div className="flash-box">
-            <div className="image">
-              <Image
-                src={Smartphone}
-                width={512}
-                height={376}
-                alt="smartphone"
-              />
-              <div className="new-badge">
-                <Badge badge={true} discount="25" />
-              </div>
-            </div>
-            <div className="content">
-              <h4 className={poppins.className + " " + " "}>Headphone</h4>
-              <h3>$120</h3>
-              <div className="review d-flex align-items-center">
-                <div className="rating d-flex align-items-center">
-                  icon
-                  <span className={poppins.className + " " + " "}>5.0</span>
-                </div>
-                <div className="sold-out">
-                  <span className={poppins.className + " " + " "}>
-                    sold out
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Col>
-        <Col md={4}>
-          <div className="flash-box">
-            <div className="image">
-              <Image
-                src={Smartphone}
-                width={512}
-                height={376}
-                alt="smartphone"
-              />
-              <div className="new-badge">
-                <Badge badge={true} discount="25" />
-              </div>
-            </div>
-            <div className="content">
-              <h4 className={poppins.className + " " + " "}>Headphone</h4>
-              <h3>$120</h3>
-              <div className="review d-flex align-items-center">
-                <div className="rating d-flex align-items-center">
-                  icon
-                  <span className={poppins.className + " " + " "}>5.0</span>
-                </div>
-                <div className="sold-out">
-                  <span className={poppins.className + " " + " "}>
-                    sold out
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Col>
+        <Col md={4}>two</Col>
+        <Col md={4}>three</Col>
       </Row>
     </div>
   );
