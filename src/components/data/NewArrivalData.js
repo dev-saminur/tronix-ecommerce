21ea33596e248e8211121105fe9c57443ac0d969
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-05 19:48:10 +0600

    Create NewArrivalData.js

---
+++ src/components/data/NewArrivalData.js
@@ -0,0 +1,66 @@
+import Smartphone from "../../assets/arrival/smartphone.jpg";
+import SmartphoneOne from "../../assets/arrival/smartphone1.jpg";
+import SmartphoneTwo from "../../assets/arrival/smartphone2.jpg";
+import SmarTv from "../../assets/arrival/smart_tv.jpg";
+import SmarTvOne from "../../assets/arrival/smart_tv1.jpg";
+import UsbSpeaker from "../../assets/arrival/usbspeaker.jpg";
+import UsbSpeakerOne from "../../assets/arrival/usbspeaker1.jpg";
+import UsbSpeakerTwo from "../../assets/arrival/usbspeaker2.jpg";
+import { MdStarRate } from "react-icons/md";
+
+export const NewArrivalData = [
+  {
+    image: Smartphone,
+    link: "/",
+    pname: "Smartphone 5G",
+    pprice: "$299.00",
+    ratingicon: <MdStarRate />,
+    review: "5.0",
+    sold: "sold 99",
+  },
+  {
+    image: UsbSpeaker,
+    link: "/",
+    pname: "USB Speaker Portable",
+    pprice: "$299.00",
+    ratingicon: <MdStarRate />,
+    review: "5.0",
+    sold: "sold 99",
+  },
+  {
+    image: SmarTv,
+    link: "/",
+    pname: "Smar Tv",
+    pprice: "$299.00",
+    ratingicon: <MdStarRate />,
+    review: "5.0",
+    sold: "sold 99",
+  },
+  {
+    image: SmartphoneOne,
+    link: "/",
+    pname: "Smartphone 5G",
+    pprice: "$299.00",
+    ratingicon: <MdStarRate />,
+    review: "5.0",
+    sold: "sold 99",
+  },
+  {
+    image: UsbSpeakerOne,
+    link: "/",
+    pname: "USB Speaker Portable",
+    pprice: "$299.00",
+    ratingicon: <MdStarRate />,
+    review: "5.0",
+    sold: "sold 99",
+  },
+  {
+    image: SmarTvOne,
+    link: "/",
+    pname: "Smart tv",
+    pprice: "$299.00",
+    ratingicon: <MdStarRate />,
+    review: "5.0",
+    sold: "sold 99",
+  },
+];
