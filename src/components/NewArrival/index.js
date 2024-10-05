1b7fca127e9d3c506609ef674668d272591c28c4
Md. Saminur Islam <saminur9125@gmail.com>
2024-10-05 16:49:05 +0600

    new-arrival-added

---
+++ src/components/NewArrival/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Header from "../helper/Header/Header";
+import Container from "../helper/Container/Container";
+import { Col, Row } from "react-bootstrap";
+import Smartphone from "../../assets/arrival/smartphone.jpg";
+import Image from "next/image";
+import Badge from "../helper/Badge/Badge";
+
+const NewArrival = () => {
+  return (
+    <>
+      <div className="arrival-section">
+        <Container>
+          <Header title="new arrival" />
+          <Row>
+            <Col md={4}>
+              <div className="arrival-box d-flex ">
+                <div className="image">
+                  <Image
+                    src={Smartphone}
+                    width={235}
+                    height={290}
+                    alt="smartphone"
+                  />
+                  <div className="new-badge">
+                    <Badge title="new" alone={true} />
+                  </div>
+                </div>
+                <div className="content">Content</div>
+              </div>
+            </Col>
+            <Col md={4}>Two</Col>
+            <Col md={4}>Three</Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default NewArrival;
